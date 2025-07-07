7bc5efe666babeb3e24150e706ce51d39075fa1a
Rakan Aditya <56795337+rakanaditya@users.noreply.github.com>
2025-07-07 00:35:38 -1200

    Update admin-auth.js

--- api/admin-auth.js
+++ api/admin-auth.js
@@ -1,11 +1,15 @@
-// /pages/api/admin-auth.js
 export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
   const { username, password } = req.body;
 
-  if (
-    username === process.env.ADMIN_USERNAME &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
+  // Cek admin tetap
+  const adminUsername = process.env.ADMIN_USERNAME;
+  const adminPassword = process.env.ADMIN_PASSWORD;
+
+  if (username === adminUsername && password === adminPassword) {
     return res.status(200).json({ isAdmin: true });
   }
 
